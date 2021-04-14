792468b1eabeb69614544e6e29c81c21845cc763
nadimnil <nadimmahmudmy@gmail.com>
2021-04-14 10:07:27 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBVlLJaqk5ZXGR1T5ov6_PzwwxYKrXKqRk",
+  authDomain: "simple-react-app25.firebaseapp.com",
+  projectId: "simple-react-app25",
+  storageBucket: "simple-react-app25.appspot.com",
+  messagingSenderId: "698061810564",
+  appId: "1:698061810564:web:c37b47339dbd58f9d24bd1"
+};
+  export default firebaseConfig;
